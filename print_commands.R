58ff1336a9abe722a53f5c54fba69a94b73a3449
mkiang <mathew.kiang@gmail.com>
2017-02-01 16:52:45 -0500

    Redid spacing on code slides

--- print_commands.R
+++ print_commands.R
@@ -1,15 +1,13 @@
+phantom <- "~/decktape-1.0.0/phantomjs "
+decktape <- "~/decktape-1.0.0/decktape.js "
+working_dir <- "~/Dropbox/Work/epi501_ta/epi501_review/"
+
 ## Print PDF of intro slides
-system(paste0("~/decktape-1.0.0/phantomjs ", 
-              "~/decktape-1.0.0/decktape.js ", 
-              "~/Dropbox/Work/epi501_ta/epi501_review/", 
-              "jan27-intro-to-odes/index.html ", 
-              "~/Dropbox/Work/epi501_ta/epi501_review/", 
-              "jan27-intro-to-odes/jan27-intro-to-r.pdf"))
+system(paste0(phantom, decktape, 
+              working_dir, "jan27-intro-to-odes/index.html ", 
+              working_dir, "jan27-intro-to-odes/jan27-intro-to-r.pdf"))
 
 ## Print PDF of measles
-system(paste0("~/decktape-1.0.0/phantomjs ", 
-              "~/decktape-1.0.0/decktape.js ", 
-              "~/Dropbox/Work/epi501_ta/epi501_review/", 
-              "jan30-timesteps-measles-seir/index.html ", 
-              "~/Dropbox/Work/epi501_ta/epi501_review/", 
-              "jan30-timesteps-measles-seir/measles-handout.pdf"))
+system(paste0(phantom, decktape, 
+              working_dir, "jan30-timesteps-measles-seir/index.html ", 
+              working_dir, "jan30-timesteps-measles-seir/measles-handout.pdf"))
